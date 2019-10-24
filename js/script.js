70b5b97d9a449128927a55b678fb9fc92042f83b
Steven <steven.scott@aggiemail.usu.edu>
2019-10-23 23:00:35 -0600

    One dataset with colors

--- js/script.js
+++ js/script.js
@@ -1,11 +1,65 @@
+let visConfig = {
+  width: 800,
+  height: 400,
+  padding: {
+    left: 30,
+    top: 50,
+    bottom: 30,
+    right: 5
+  }
+};
+
 
 // load the demo
-d3.csv('data/demo3.csv').then(function(data) {
+d3.csv('data/demo3.csv').then(function (data) {
   console.log('Data', data);
   let timeScale = d3.scaleLinear()
-      .domain([0, d3.max(data, d => {
-        return d['t'];
-      })])
-      .range([0, 100])
-      ;
+    .domain([0, d3.max(data, d => {
+      return parseFloat(d['t']);
+    })])
+    .range([visConfig.padding.left, visConfig.width - visConfig.padding.right])
+    ;
+
+  let axis = d3.select('body')
+    .append('svg')
+    .attr('width', visConfig.width)
+    .attr('height', visConfig.height)
+    .append('g')
+    .attr('transform', `translate(${visConfig.padding.left}, ${visConfig.height - visConfig.padding.bottom})`)
+    ;
+
+  let bottomAxis = d3.axisBottom()
+    .scale(timeScale);
+
+  axis.call(bottomAxis);
+
+    axis.selectAll('circle')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('cx', (d) => {
+      return timeScale(parseFloat(d['t']));
+    })
+    .attr('cy', -50)
+    .attr('r', 3)
+    .style('fill', (d) => {
+      switch(d['event type']) {
+        case 'init': 
+          return 'red';
+        case 'collision':
+          return 'green';
+        case 'beta = 0':
+          return 'violet';
+        case 'theta = 0':
+          return 'brown';
+        case 'phi = 0':
+          return 'orange';
+        case 'pphi = 0':
+          return 'purple';
+        default:
+          return 'black';
+      }
+    })
+    ;
+
 });
