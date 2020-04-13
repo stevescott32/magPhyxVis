a937e846fd8fb1be958b4c46d6225a289ee374b2
Jaxon Willard <jaxonbwillard@gmail.com>
2020-04-13 11:27:26 -0600

    refactoring unused code

--- c#/Main.cs
+++ c#/Main.cs
@@ -13,12 +13,6 @@ namespace Main
         static void Main(string[] args)
         {
 
-
-
-
-
-    
-
             List<Tuple<string,BigInteger,int>> commandLines_HI_index = new List<Tuple<string, BigInteger,int>>();
             for (int i=0; i<100; i++){
                 string index;
@@ -49,10 +43,10 @@ namespace Main
                 data[0] = tuples[i].Item1;
                 System.IO.File.WriteAllLines(address, data);
             }
-            correlateEventsAnd_Commands(tuples);
+            correlateEventsAndCommands(tuples);
         }
 
-        public static void correlateEventsAnd_Commands(List<Tuple<string, BigInteger, int>> tuples){
+        public static void correlateEventsAndCommands(List<Tuple<string, BigInteger, int>> tuples){
             for (int i=0; i<tuples.Count; i++){
                 string index;
                 string tuplesIndex;
@@ -116,67 +110,6 @@ namespace Main
             return returnLine;
 
         }
-
-        public static void createNewFileOfArrangedEventCoordinates(string address){
-            List<Tuple<string, BigInteger>> coordsAndHindex = linesAndHI(address);
-
-            coordsAndHindex.Sort((a, b) => a.Item2.CompareTo(b.Item2));
-
-            string[] arrangedCoordinates = arrangedEventCoordinateString(coordsAndHindex);
-
-            System.IO.File.WriteAllLines(address, arrangedCoordinates);
-
-
-        }
-
-        public static string[] arrangedEventCoordinateString(List<Tuple<string, BigInteger>> coordsAndHindex){
-            string[] arrangedCoords = new string[coordsAndHindex.Count];
-            for (int i=0; i<coordsAndHindex.Count; i++){
-                arrangedCoords[i] = coordsAndHindex[i].Item1;
-            }
-            
-
-            return arrangedCoords;
-        }
-
-        public static List<Tuple<string, BigInteger>> linesAndHI(string address){
-            string[][] parsedData = readEventsFile(address);
-            /*
-            TODO: find out how bits per dimension works!"
-            */
-            int bpd = FindBitsPerDimension(800);
-
-            /*
-            TODO: dynamically create this number!!
-            */
-            float SCALAR = (float)Math.Pow(10,6);
-    
-           List<Tuple<string, BigInteger>> linesAndHIndex = new List<Tuple<string, BigInteger>>();
-            // for each line in data file
-            for (int i=1; i<parsedData.Length; i++){
-                var coords = parsedData[i][2..(parsedData[i].Length)];
-                int[] scaledIntCoordinates = new int[coords.Length];
-
-                // for each coordinate in line, cast to float and scale so everything is an integer (hilbert point requires integers)
-                for (int j=0; j<coords.Length; j++){
-                    float floatCoordinate = (float)Double.Parse(coords[j], System.Globalization.NumberStyles.Float);
-                    float scaledCoordinate = floatCoordinate * SCALAR;
-                    int coordinate = (int)scaledCoordinate;
-                    scaledIntCoordinates[j] = coordinate;
-                }
-                // create new list, fill indices with original line and append hilbert index
-                string[] coordinatesWithAppendedHilbertIndex = new string[parsedData[i].Length + 1];
-                for (int j=0; j<parsedData[i].Length; j++){
-                    coordinatesWithAppendedHilbertIndex[j] = parsedData[i][j];
-                }
-                HilbertPoint hilbertPoint = new HilbertPoint(scaledIntCoordinates, bpd);
-
-                var tuple = new Tuple<string, BigInteger>(  lineToString(parsedData[i])  , hilbertPoint.HilbertIndex );  
-
-                linesAndHIndex.Add(tuple);   
-            }   
-            return linesAndHIndex;
-        }
         public static string lineToString(string[] line){
             string toReturn = "";
             for (int i=0; i<line.Length; i++){
@@ -184,30 +117,6 @@ namespace Main
             }
             return toReturn;
         }
-
-        public static string[][] readEventsFile(string address){
-            string[][] lines;
-            string[] lines1 = System.IO.File.ReadAllLines(address);
-            using (TextFieldParser parser = new TextFieldParser(address)){
-            lines = new string[lines1.Length][];
-            parser.Delimiters = new string[] { "," };
-            int index = 0;
-            while (true)
-            {
-                string[] parts = parser.ReadFields();
-                if (parts == null)
-                {
-                    break;
-                }
-                lines[index] = parts;
-                index++;
-            }
-        }
-        return lines;
-        }
-
-
-
 
         public static int FindBitsPerDimension(int max)
 		{
